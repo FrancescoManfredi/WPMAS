c6be96bdad12448416883e9b7f337b89d8ff2b9b
Francesco Manfredi <francescomanfrediwd@gmail.com>
2017-07-07 15:19:09 +0200

    Start

---
+++ wp-multiagentsystem.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP-Multi Agent System Communication
+Plugin URI:  https://github.com/FrancescoManfredi/WPMAS
+Description: Send events notifications to a Multi Agent System in FIPA ACL
+Version:     20160911
+Author:      Francesco Manfredi
+Author URI:  http://www.francescomanfredi.it
+*/
+
+// Avoid standalone execution
+defined( 'ABSPATH' ) or die( 'This is a worpdress plugin. You should not execute it.' );
