2f8ee5ea89c87286dc79ebeb43499d01a278c53d
Francesco Manfredi <francescomanfrediwd@gmail.com>
2017-07-07 17:34:48 +0200

    Database functionalities stubs

---
+++ wpmas-db.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * Functionalities related to the database
+ */
+
+/**
+ * Get plugin options
+ * Returns all options as a dictionary
+ */
+function wpmas_get_options() {
+	
+}
+
+/**
+ * Test for plugin database table existence
+ * Returns true if exists
+ */
+function wpmas_db_exists() {
+	
+}
